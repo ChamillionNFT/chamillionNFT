6b134e4bf9fc0fce9b0de8c1a7308c5ed9c488cb
ChamillionNFT <chamillionnft@gmail.com>
2021-10-21 14:52:27 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home() {
             <div className="launch_date">
             
               <p>
-            <strong>LAUNCH DATE:</strong> November 25th
+            <strong>LAUNCH DATE:</strong> November 15th
             </p>
               <p>
 <strong>TOTAL SUPPLY:</strong> 5,555 Chamillions
