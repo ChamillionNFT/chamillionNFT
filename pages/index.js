094544d9653235f0ea37d36fe52976f0a65293aa
ChamillionNFT <91974561+ChamillionNFT@users.noreply.github.com>
2021-11-03 02:01:10 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -142,6 +142,7 @@ export default function Home() {
          <Link href="https://twitter.com/DaltonStearns"><a target="_blank" rel="noreferrer">@Dalton</a></Link>
          </div>
          </p>
+         <p>
          <div  className="profile">
            <div>
              <Image className="profile_img" src="/Profile Pic1.jpg" alt="profile picture"
@@ -151,6 +152,8 @@ export default function Home() {
            <h3 className="title">Design Lead</h3>
          <Link href="https://twitter.com/CommonNFT"><a target="_blank" rel="noreferrer">@CommonNFT</a></Link>
          </div>
+         </p>
+         
          <div  className="profile">
            <div>
              <Image className="profile_img" src="/Profile Pic2.jpg" alt="profile picture"
